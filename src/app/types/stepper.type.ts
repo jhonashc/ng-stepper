e13409114eae3b659d83d7322b85d787321f27ef
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-04-23 23:54:24 -0500

    stepper type updated: step type was modified

--- src/app/types/stepper.type.ts
+++ src/app/types/stepper.type.ts
@@ -1,7 +1,11 @@
-import { DetailsStep, PreferencesStep } from '../interfaces/stepper.interface';
+import {
+  DetailsStep,
+  PreferencesStep,
+  StepperState,
+} from '../interfaces/stepper.interface';
 
 export type StepDataActions =
   | { stepName: 'details'; data: DetailsStep }
   | { stepName: 'preferences'; data: PreferencesStep };
 
-export type StepType = Pick<StepDataActions, 'stepName'>;
+export type StepType = keyof StepperState['steps'];
