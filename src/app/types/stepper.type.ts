c4399e08e09d79d74ca72caff78506d25b8bf2c9
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-04-24 09:27:57 -0500

    stepper types updated: step data actions was removed

--- src/app/types/stepper.type.ts
+++ src/app/types/stepper.type.ts
@@ -1,11 +1,3 @@
-import {
-  DetailsStep,
-  PreferencesStep,
-  StepperState,
-} from '../interfaces/stepper.interface';
+import { StepperState } from '../interfaces/stepper.interface';
 
-export type StepDataActions =
-  | { stepName: 'details'; data: DetailsStep }
-  | { stepName: 'preferences'; data: PreferencesStep };
-
-export type StepType = keyof StepperState['steps'];
+export type StepName = keyof StepperState['steps'];
